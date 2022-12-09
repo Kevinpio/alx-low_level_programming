9f2475bbbaa95de519957dce40eec526296e6438
Kevinpio <kevinpioani2@gmail.com>
2022-12-08 18:02:02 -0800

    Another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Description: use puts to output a string
- *
- *     * Return: Always 0 (success)
- *
- *      */
+/*
+ *   main - Entry point
+ *   Description: use puts to output a string
+ *   Return: Always 0 (success)
+ */
 
 int main(void)
 
 {
 
-		char *exp;
+	char *exp;
 
 
-
-			exp = "\"Programming is like building a multilingual puzzle";
+		exp = "\"Programming is like building a multilingual puzzle";
 
 				puts(exp);
 
