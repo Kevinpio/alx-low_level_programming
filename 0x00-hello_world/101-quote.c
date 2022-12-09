20b8a3f5fa70e79d83df97582f41bcd9e25b86e6
Kevinpio <kevinpioani2@gmail.com>
2022-12-09 14:15:22 -0800

    Commit for Correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,15 @@
 /**
  * main - Entry point
  *
- * Description - program to print to standard error withoout using printf or puts
+ * Description - program to print to standard error withoout using printf
+ *
+ * or puts
  *
  * Return: 1 (failure)
  *
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
