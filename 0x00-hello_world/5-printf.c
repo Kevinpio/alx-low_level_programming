4b5d42de93abc3ea4fa18099337b2772ad854cb2
Kevinpio <kevinpioani2@gmail.com>
2022-12-09 01:39:58 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-
 /**
- *
  * main - Entry point
  *
- * 
+ * Description: use 'printf' to print a string and a new line
  *
- *  Description: use 'printf' to print a string and a new line
- *
- *  Return: Always 0 (success)
- *
- *           */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+		printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
 }
