9ab628b83a7f9a4b3e27dc59d8e673d7c4b429bc
Kevinpio <kevinpioani2@gmail.com>
2022-12-09 01:31:11 -0800

    Finallyyyyyy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-
-/*
+/**
  * main - Entry point
  *
- * Description: use puts to output a string
- * Return: Always 0 (success)
-*/
-
- /* main.c */   int main(void)
-
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-		char *exp;
-
-			exp = "\"Programming is like building a multilingual puzzle";
-
-				puts(exp);
-
-					return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
