13058024d5dfc7040a67f83806198558e17e2913
Kevinpio <kevinpioani2@gmail.com>
2022-12-12 11:54:03 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descrition - a program that prints the alphabet in lowercase,
+ *
+ * followed by a new line.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int k;
+		k = 0;
+		while (k < 10)
+		{
+		putchar(k);
+		if (k == 9)
+		putchar('\n');
+		k++;
+		}
+		return (0);
+}
