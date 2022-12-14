6a8f70828352ddf97eab0702d11335c8bd0993b9
Kevinpio <kevinpioani2@gmail.com>
2022-12-14 08:33:51 -0800

    Omooo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,13 +11,14 @@
  */
 int main(void)
 {
-		char c[10] = "_putchar";
-		int i;
-			i = 0;
-			while (c[i])
-			_putchar(c[i]);
-			i++;
-			}
-			_putchar('\n');
-			return (0);
+	char c[10] = "_putchar";
+	int i;
+		{
+		i = 0;
+		while (c[i])
+		_putchar(c[i]);
+		i++;
+		}
+		_putchar('\n');
+		return (0);
 }
