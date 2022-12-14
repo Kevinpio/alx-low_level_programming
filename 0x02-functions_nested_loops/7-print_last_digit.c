8fac1fbfccf4052501ba92cbc999baa268dc313c
Kevinpio <kevinpioani2@gmail.com>
2022-12-14 13:08:10 -0800

    Ommooo correct

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,9 @@
 int print_last_digit(int n)
 {
 	int r;
-	r = n % 10;
-	if (r < 0)
-	r = -r;
-	_putchar(r + '0');
-	return (r);
+		r = n % 10;
+		if (r < 0)
+		r = -r;
+		_putchar(r + '0');
+		return (r);
 }
