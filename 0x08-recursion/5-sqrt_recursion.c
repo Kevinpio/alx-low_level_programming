fe97f27c7466daf9f450675e409daebcd0e4d17a
Kevinpio <kevinpioani2@gmail.com>
2023-01-06 01:14:06 -0800

    correct

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,9 +9,9 @@ int power_operation(int x, int y)
 {
 	if ((y * y) == x)
 		return (y);
-		if (y == x)
+	if (y == x)
 		return (-1);
-		return (0 + power_operation(x, y + 1));
+	return (0 + power_operation(x, y + 1));
 }
 /**
  * _sqrt_recursion - square root
@@ -23,9 +23,9 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-		if (n == 0)
+	if (n == 0)
 		return (0);
-		if (n == 1)
+	if (n == 1)
 		return (1);
-		return (power_operation(n, 2));
+	return (power_operation(n, 2));
 }
