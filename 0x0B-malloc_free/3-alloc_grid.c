03c74ac159b49fd2980361e5700f5b25df6eedbb
Kevinpio <kevinpioani2@gmail.com>
2023-01-10 17:21:57 -0800

    3-alloc correct

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,8 +23,8 @@ int **alloc_grid(int width, int height)
 	if (matrix[i] == NULL)
 		{
 	for ( ; i >= 0; )
-		free(matrix[i--]);
-		free(matrix);
+	free(matrix[i--]);
+	free(matrix);
 		return (NULL);
 		}
 	for (j = 0; j < width; j++)
