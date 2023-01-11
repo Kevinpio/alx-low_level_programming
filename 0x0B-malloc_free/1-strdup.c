34d786327eb6a0a3a1814565404d5d87d5ec9df1
Kevinpio <kevinpioani2@gmail.com>
2023-01-10 17:03:42 -0800

    Temp

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 		char *p;
 	if (str == NULL)
 		return (NULL);
-		for (i = 0; str[i]; i++)
+	for (i = 0; str[i]; i++)
 		p = malloc(sizeof(char) * i + 1);
 		if (p == NULL)
 		return (NULL);
