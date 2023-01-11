6b03176efb6a1818f4145586489c9cb0d4403864
Kevinpio <kevinpioani2@gmail.com>
2023-01-10 17:27:26 -0800

    100-argst correct

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,13 +14,13 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0, total = 0; i < ac; i++)
 	for (j = 0; av[i][j]; j++, total++)
-		total += ac;
+	total += ac;
 	p = malloc(total * sizeof(char) + 1);
 	for (i = 0; i < ac; i++)
 		{
-	for (j = 0; av[i][j]; j++)
-	*p++ = av[i][j];
-	*p++ = '\n';
+		for (j = 0; av[i][j]; j++)
+		*p++ = av[i][j];
+		*p++ = '\n';
 		}
 		p -= total;
 		return (p);
